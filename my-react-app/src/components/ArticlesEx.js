1453ec9bf52629f6f0933e593da41fc15c482383
Henry <henrypham20@tamu.edu>
2021-11-07 11:45:13 -0600

    Added React files

---
+++ my-react-app/src/components/ArticlesEx.js
@@ -0,0 +1,22 @@
+
+/*const [articles, setArticles] = useState([
+    {
+        id: 1,
+        title: 'Alec Baldwin\'s shooting accident likened to Jon-Erik Hexum, Brandon Lee’s deaths',
+        date: 'October 22, 2021',
+        source: 'Fox News',
+    },
+    {
+        id: 2,
+        title: 'At least 8 dead and many injured after crowd surge at Travis Scott\'s Astroworld Festival in Houston, officials say',
+        date: "November 6, 2021",
+        source: 'CNN',
+    },
+    {
+        id: 3,
+        title: 'Iraqi PM al-Kadhimi survives Baghdad \'assassination attempt\'',
+        date: 'November 6, 2021',
+        source: 'BBC News',
+    },
+])
+*/
