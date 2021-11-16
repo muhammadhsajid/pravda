579e45aee54b77dbf9be86db26743697a76956eb
msajid <16084+msajid@users.noreply.github.tamu.edu>
2021-11-16 09:50:02 -0600

    User Rec v1.2

--- newsalgo.py
+++ newsalgo.py
@@ -4,10 +4,13 @@
 #news to rating as dict?
 #1-5
 import math
+import random
 #20 News Sources
-ns = ["NBC", "CBS","CNN", "FoxNews", "Reuters", "Business Insider", "New York Post"
-        ,"The Hill", "Associated Press", "Huffington Post", "The New York Times", "Politico"
-        , "The Guardian", "The Washington Post", "Wall Street Journal"]
+ns = [["Huffington Post", "Buzzfeed News"]
+        ,["CNN", "The Guardian", "Politico", "NBC News", "CBS News", "The New York Times"]
+        ,["Reuters", "Associated Press", "The Economist", "The Atlantic", "npr", "pbs", "c-span"]
+        ,["Fox News", "The Hill", "The Washington Post", "Business Insider", "The Wall Street Journal"]
+        ,["New York Post"]]
 #METHODOLOGY
 '''
 ways to suggest user content?
@@ -17,18 +20,41 @@ number-based (rank articles viewed vs bias as a number and suggest stdev  outsid
 
 '''
 def biasadj(userbias):
-    userbias = 1.8752 #just temp val for now, replace by using json later and calcing avg value;
+    #just temp val for now, replace by using json later and calcing avg value;
     bias_adj_score = []
     #positive adj:
+    bias_adj_score.append(userbias-(math.sqrt(-(0-userbias)))/5)
     bias_adj_score.append(userbias+(math.sqrt(5-userbias)/5))
     #negative adj:
-    bias_adj_score.append(userbias-(math.sqrt(-(0-userbias)))/5)
     return(bias_adj_score)
-def drawing(drawmat):
+def drawing(userbias):
+    normal = biasadj(userbias)
+    heavyl = False
+    heavyr = False
+    adjbiasscore = round(userbias)
+    realadjscores = []
+
+    if(userbias-1 < 1):
+        #print("User is Biased heavily")
+        heavyl = True
+        realadjscores = [2,4]
+        print(realadjscores)
+    elif(userbias-4 < 1):
+        heavyr = True
+        realadjscores = [2,4]
+        print(realadjscores)
+    #6 favored articles, 7 l bias, 7 r bias
+    overallrec = []
+    brec = []
+    for i in range(0,5):
+        brec.append(random.choice(ns[adjbiasscore-1]))
+    overallrec.append(brec)
+    print(overallrec)
+    print(adjbiasscore)    
     return 0
 def ranking(bias_adj_score):
     return 0
 def topics(rankmat):
     return 0
     
-
+drawing(4.512)
